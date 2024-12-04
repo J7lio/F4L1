0d9602876a14274a28f53a54acef540b30f14da0
J7lio <juliodela.jm@gmail.com>
2024-12-04 18:23:21 +0100

    al codigo S_temporal_async_prueba.py cambiado de 0.0.0.0 a localhost y añadida la variable de timestamp

--- tests/S_temporal_async_prueba.py
+++ tests/S_temporal_async_prueba.py
@@ -9,14 +9,16 @@ async def main():
     server = Server()
     server._application_uri = "urn:f4l1.servers:tiempo_simulado"
     await server.init()
-    server.set_endpoint("opc.tcp://0.0.0.0:4840/freeopcua/servidor_temporal/")
+    server.set_endpoint("opc.tcp://localhost:4840/freeopcua/servidor_temporal/")
 
     uri = "http://www.f4l1.es/server/temporal"
     idx = await server.register_namespace(uri)
 
-    sim_time_obj = await server.nodes.objects.add_object(idx, "servidor_temporal")
-    sim_time_var = await sim_time_obj.add_variable(idx, "Hora_simulada", "29-10-24 00:00")
+    sim_time_obj = await server.nodes.objects.add_object(idx, "ServidorTemporal")
+    sim_time_var = await sim_time_obj.add_variable(idx, "HoraSimulada", "29-10-24 00:00")
+    sim_time_timestamp = await sim_time_obj.add_variable(idx, "HoraSimuladaTimestamp", 0.0)
     await sim_time_var.set_writable()
+    await sim_time_timestamp.set_writable()
 
     simulated_time = datetime(2024, 10, 29, 0, 0)
     time_step = timedelta(minutes=5)  # Incremento de 5 minutos simulados
@@ -29,6 +31,7 @@ async def main():
             formatted_time = simulated_time.strftime('%d-%m-%y %H:%M')  # Convertir a formato del csv
             _logger.info("Hora simulada actualizada: %s", formatted_time)
             await sim_time_var.write_value(formatted_time)
+            await sim_time_timestamp.write_value(simulated_time.timestamp())
 
             await asyncio.sleep(real_step_duration)
 
